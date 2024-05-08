fb2525b963c44881c0174345541b015833c02aa9
sallyzy0123 <sally_zy@protonmail.com>
2024-05-08 20:04:59 +0300

    update graphql

--- src/api/schemas/index.ts
+++ src/api/schemas/index.ts
@@ -2,7 +2,9 @@ import path from 'path';
 import {loadFilesSync} from '@graphql-tools/load-files';
 import {mergeTypeDefs} from '@graphql-tools/merge';
 
-const typesArray = loadFilesSync(path.join(__dirname, './**/*.graphql'));
+const typesArray = loadFilesSync('./*/', {
+  extensions: ['graphql'],
+});
 const typeDefs = mergeTypeDefs(typesArray);
 
 export default typeDefs;
